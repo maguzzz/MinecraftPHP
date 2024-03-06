10457f933f3b0b009319e7456222180e86470af7
PlutoProgram <asdasdasd>
2024-03-06 10:36:53 +0100

    Tools Material & uniqueName in Items

--- Parents/Tools.php
+++ Parents/Tools.php
@@ -1,14 +1,25 @@
 <?php
 include_once('Items.php');
+
+enum Materials 
+{
+    case Wood;
+    case Stone;
+    case Iron;
+    case Gold;
+    case Diamond;
+}
+
 abstract class Tools extends Items {
-
-    
-
 
     private $material;
 
-    function __construct() {
+    function __construct(Materials $materials) {
+
         $this->setMaxCount(1);
+
+               
+
     }
 
 }
