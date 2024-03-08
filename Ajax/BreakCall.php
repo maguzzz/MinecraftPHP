08e8b4ab8d81d54117338581455e781a044d02fd
maguzzz <markusm.touray@gmail.com>
2024-03-08 08:18:48 +0100

    Get Js Data

--- Ajax/BreakCall.php
+++ Ajax/BreakCall.php
@@ -1,2 +1,13 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the data from the POST request
+    $postData = json_decode(file_get_contents('php://input'), true);
+
+    // Echo back the received data
+    echo json_encode($postData);
+} else {
+    // Handle other HTTP methods if necessary
+    http_response_code(405); // Method Not Allowed
+    echo "Only POST requests are allowed for this endpoint";
+}
